54ca4cbb86fe3367ee06de8cae0306efd676e23c
Utsav Sethi <utsavsethi@gmail.com>
2017-06-20 15:48:51 -0500

    15/16

--- src/koans/15_destructuring.clj
+++ src/koans/15_destructuring.clj
@@ -27,18 +27,19 @@
   "You can regain the full argument if you like arguing"
   (= {:original-parts ["Stephen" "Hawking"] :named-parts {:first "Stephen" :last "Hawking"}}
      (let [[first-name last-name :as full-name] ["Stephen" "Hawking"]]
-       __))
+       {:original-parts full-name :named-parts {:first first-name :last last-name}}))
 
   "Break up maps by key"
   (= "123 Test Lane, Testerville, TX"
      (let [{street-address :street-address, city :city, state :state} test-address]
-       __))
+       (str street-address ", " city ", " state)))
 
   "Or more succinctly"
   (= "123 Test Lane, Testerville, TX"
-     (let [{:keys [street-address __ __]} test-address]
-       __))
+     (let [{:keys [street-address city state]} test-address]
+       (str street-address ", " city ", " state)))
 
   "All together now!"
   (= "Test Testerson, 123 Test Lane, Testerville, TX"
-     (___ ["Test" "Testerson"] test-address)))
+     ((fn [[first-name last-name] {:keys [street-address city state]}]
+        (str first-name " " last-name ", " street-address ", " city ", " state)) ["Test" "Testerson"] test-address)))
